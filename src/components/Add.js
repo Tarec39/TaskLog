eb6c19731218ae83ec8ab53c9a1590e7bbafca6c
Tarek <itcameranian@gmail.com>
2021-08-21 23:58:02 +0900

    [update] デザインを起こしている

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,73 +1,30 @@
-import React from 'react';
+import React from 'react'
 
-const AddTaskLog = (props)=>{
-    const { addTaskGroupName, addTask, addedTasks, handleChange, handleSubmit, handleEnterKey , isEnterPressed, test} = props
+const AddTaskLogs = () =>{
     return(
-        <div className="addTaskLog">
-            <form>
-                <AddTaskGroupName
-                value={addTaskGroupName}
-                onChange={handleChange}
+        <div className="addTaskLogs">
+            <div className="addTaskGroupName">
+                <input
+                placeholder="タイトル"
                 />
-                {isEnterPressed === true &&
-                <AddedTasks addedTasks={addedTasks}/>
-                }
-                <AddTasks
-                value={addTask}
-                onChange={handleChange}
-                onKeyPress={handleEnterKey}
-                addedTasks={addedTasks}
+            </div>
+            <div className="addedTasks">
+                <ul>
+                    <li className="addedTask">
+                        asdasdas
+                    </li>
+                </ul>
+            </div>
+            <div className="addTask">
+                <input
+                placeholder="+   タスクを追加..."
                 />
-
-                <button type="submit" onClick={handleSubmit}>登録</button>
-            </form>
-            {/* <LogBtn onClick={test}/> */}
-        </div>
-    )
-}
-const AddTaskGroupName = ({value, onChange}) =>{
-    return(
-        <input
-        className="addTaskGroupName"
-        name="addTaskGroupName"
-        type="text"
-        placeholder="タイトル"
-        value={value}
-        onChange={onChange} 
-        />
-    )
-}
-const AddTasks = ({value, onChange, onKeyPress}) =>{
-    return(
-        <div className="addTasks">
-            <input
-            type="text"
-            name="addTask"
-            className="addTask"
-            placeholder="+ タスクを追加"
-            value={value}
-            onChange={onChange}
-            onKeyPress={onKeyPress}
-            />
+            </div>
+            <div className="addTaskLogBtn">
+                <p>登録</p>
+            </div>
         </div>
     )
 }
 
-const AddedTasks = ({addedTasks}) =>{
-    return(
-        <ul className="addedTasks">
-            {Object.keys(addedTasks).map((key, i) =>(
-                <li key={i}>{addedTasks[key]}</li>
-            ))}
-        </ul>
-    )
-}
-
-const LogBtn = ({onClick}) =>{
-    return(
-        <div>
-            <button onClick={onClick}>LOG</button>
-        </div>
-    )
-}
-export default AddTaskLog;
+export default AddTaskLogs
