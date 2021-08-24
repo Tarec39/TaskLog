c82ed7d052b621e7513c9181399abe0b1eae8169
Tarek <itcameranian@gmail.com>
2021-08-24 13:39:23 +0900

    [update] テーブルが動くぞおおおお

---
+++ src/components/TaskLog.js
@@ -0,0 +1,197 @@
+import React, {useState} from 'react'
+
+const TaskLogs = () =>{
+    return(
+        <div className="TaskLog">
+            <div class="TaskGroupName">サンプルなタイトル</div>
+            <div className="Content">
+            <table>
+                <tr className="Dates">
+                    <th className="Blank"></th>
+                    <th className="Date">8/1</th>
+                    <th className="Date">8/2</th>
+                    <th className="Date">8/3</th>
+                    <th className="Date">8/4</th>
+                    <th className="Date">8/5</th>
+                    <th className="Date">8/6</th>
+                    <th className="Date">8/7</th>
+                    <th className="Date">8/7</th>
+                    <th className="Date">8/7</th>
+                    <th className="Date">8/7</th>
+                    <th className="Date">8/7</th>
+                    <th className="Date">8/7</th>
+                    <th className="Date">8/7</th>
+                    <th className="Date">8/7</th>
+                    <th className="Date">8/7</th>
+                </tr>
+                <tr className="TasknData">
+                    <th className="Task">サンプルタスク①</th>
+                    <td className="Data"><input/></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                </tr>
+                <tr className="TasknData">
+                    <th className="Task">サンプルタスク①</th>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                </tr>
+                <tr className="TasknData">
+                    <th className="Task">サンプルタスク①</th>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                </tr>
+                <tr className="TasknData">
+                    <th className="Task">サンプルタスク①</th>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                </tr>
+                <tr className="TasknData">
+                    <th className="Task">サンプルタスク①</th>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                </tr>
+                <tr className="TasknData">
+                    <th className="Task">サンプルタスク①</th>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                </tr>
+                <tr className="TasknData">
+                    <th className="Task">サンプルタスク①</th>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                </tr>
+                <tr className="TasknData">
+                    <th className="Task">サンプルタスク①</th>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                </tr>
+                <tr className="TasknData">
+                    <th className="Task">サンプルタスク①</th>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                    <td className="Data"></td>
+                </tr>
+
+            </table>
+            </div>
+
+        </div>
+    )
+}
+
+export default TaskLogs
